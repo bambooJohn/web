db4c5ba47127293639c2581a4a959ab650b3692d
liangzhen <1033855573@qq.com>
2020-12-10 08:05:53 +0800

    05Jsp简介&Helloworld

---
+++ workspace-sts-3.9.4.RELEASE/day07_jsp/src/com/bambooJohn/demo/Demo.java
@@ -0,0 +1,14 @@
+package com.bambooJohn.demo;
+
+public class Demo {
+
+	/**
+	 * Jsp
+	 * 		* 简介：Java Server Pages (java服务器页面)
+	 * 			* Servlet = java + html
+	 * 			* Jsp = html + java
+	 * 			* Jsp只能运行服务器（Web容器）中。
+	 * 		* Jsp本质是Servlet
+	 */
+	
+}
