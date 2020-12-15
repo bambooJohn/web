efbaa2424d354a2e1029a2fb879bdae1f5c70c5c
liangzhen <1033855573@qq.com>
2020-12-15 15:25:21 +0800

    02JSTL通用标签

---
+++ workspace-sts-3.9.4.RELEASE/day09_jstl/src/com/bambooJohn/demo/Demo.java
@@ -0,0 +1,21 @@
+package com.bambooJohn.demo;
+
+public class Demo {
+
+	/*
+	 * JSTL
+	 * 		简介：JSP Standard Tag Library（JSP标准标签库）
+	 * 		作用：
+	 * 			JSTL替代JSP中的脚本中的代码。
+	 * 		使用JSTL
+	 * 			导包 	taglibs-standard-impl-1.2.1.jar
+	 * 					taglibs-standard-spec-1.2.5.jar
+	 * 				<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+	 * 			使用：
+	 * 		JSTL常用标签
+	 * 			
+	 * 
+	 * 
+	 */
+	
+}
