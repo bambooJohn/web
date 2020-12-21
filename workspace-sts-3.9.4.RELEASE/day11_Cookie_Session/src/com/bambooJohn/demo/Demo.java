f420a679f50ba2bf7308b6713973bf3725612dfb
liangzhen <1033855573@qq.com>
2020-12-21 13:36:58 +0800

    01Cookie简介&运行原理

---
+++ workspace-sts-3.9.4.RELEASE/day11_Cookie_Session/src/com/bambooJohn/demo/Demo.java
@@ -0,0 +1,24 @@
+package com.bambooJohn.demo;
+
+public class Demo {
+
+	/**
+	 * Cookie
+	 * 		* 简介
+	 * 			* Cookie实际上就是服务器保存在浏览器上的一段信息,主要用于区分不同的用户。
+	 * 		* Cookie运行原理
+	 * 			* 请求
+	 * 			* 服务器创建一个Cookie对象，该Cookie对象携带用户信息，服务器发送（响应）给客户端。
+	 * 			* 以后客户端再发送请求时，会携带该Cookie对象。
+	 * 			* 服务器会根据该Cookie对象（及信息），区分不同用户。
+	 * 		* Cookie创建&获取&修改
+	 * 		* Cookie有效性
+	 * 		* Cookie有效路径
+	 * 		* Cookie应用
+	 * 		* Cookie缺陷
+	 * 
+	 * Session
+	 * 
+	 */
+	
+}
