cbfd69cb73c96a08e38ea02d6ef3a8c6c82a13a2
liangzhen <1033855573@qq.com>
2020-12-21 15:11:35 +0800

    update .gitignore

---
+++ workspace-sts-3.9.4.RELEASE/day11_Cookie_Session/src/com/bambooJohn/servlet/CreateCookieServlet.java
@@ -0,0 +1,29 @@
+package com.bambooJohn.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CreateCookieServlet
+ */
+public class CreateCookieServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//创建Cookie对象，并携带用户信息
+		Cookie cookieName = new Cookie("stuName", "zhangsan");
+		//响应给客户端
+		response.addCookie(cookieName);
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
