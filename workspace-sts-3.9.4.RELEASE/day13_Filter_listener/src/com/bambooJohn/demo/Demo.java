6f7945f7470da321c2f7a3051a50d0bd39556186
liangzhen <1033855573@qq.com>
2020-12-28 08:00:26 +0800

    01Filter简介&FilterHelloworld

---
+++ workspace-sts-3.9.4.RELEASE/day13_Filter_listener/src/com/bambooJohn/demo/Demo.java
@@ -0,0 +1,29 @@
+package com.bambooJohn.demo;
+
+public class Demo {
+
+	/**
+	 * Filter:过滤器
+	 * 	* 简介：程序的过滤器，过滤客户端与服务器之间的请求和响应。
+	 * 		* 如：过滤字符集
+	 * 		* Filter是web三大核心组件之一
+	 * 			* Servlet:处理用户请求
+	 * 			* Filter:过滤用户请求&响应
+	 * 			* Listener:监听器
+	 * 		* web三大核心组件的共同点
+	 * 			1. 实现某个接口
+	 * 			2. 注册
+	 * 	* HelloWorld
+	 * 	* Filter生命周期
+	 * 	* Filter的工作原理
+	 * 	* 多个Filter的执行流程
+	 * 	* Url的配置规则
+	 * 	* HttpFilter
+	 * 
+	 * 生活中的过滤器
+	 * 	* 自来水
+	 * 	* 空气
+	 * 
+	 */
+	
+}
