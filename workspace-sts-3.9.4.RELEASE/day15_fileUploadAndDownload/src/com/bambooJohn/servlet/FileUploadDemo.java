b606fa8ae6e8f8b8e1a3fe524c26916f47131b6e
liangzhen <1033855573@qq.com>
2021-01-02 16:57:20 +0800

    06优化文件上传（同名问题&单文件大小）

--- workspace-sts-3.9.4.RELEASE/day15_fileUploadAndDownload/src/com/bambooJohn/servlet/FileUploadDemo.java
+++ workspace-sts-3.9.4.RELEASE/day15_fileUploadAndDownload/src/com/bambooJohn/servlet/FileUploadDemo.java
@@ -2,7 +2,9 @@ package com.bambooJohn.servlet;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
+import java.util.UUID;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -10,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.fileupload.FileItem;
-
+import org.apache.commons.fileupload.FileUploadBase.FileSizeLimitExceededException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
@@ -29,12 +31,15 @@ public class FileUploadDemo extends HttpServlet {
 		 * 	3.使用ServletFileUpload中的List<FileItem> parseRequest(request)
 		 * 	4.使用FileItem中的write()方法，写到服务器。
 		 */
-		
+		response.setContentType("text/html;charset=UTF-8");
+		request.setCharacterEncoding("UTF-8");
+		PrintWriter writer = response.getWriter();
 		//1.创建工厂类
 		DiskFileItemFactory fileItemFactory = new DiskFileItemFactory();
 		//2.创建解析器ServletFileUpload//ServletFileUpload
 		ServletFileUpload fileUpload = new ServletFileUpload(fileItemFactory);
 		//获取upload真实路径
+		fileUpload.setFileSizeMax(2*1024);
 		String realPath = this.getServletContext().getRealPath("/upload");
 		try {
 			//3.使用ServletFileUpload中的List<FileItem> parseRequest(request)
@@ -42,17 +47,22 @@ public class FileUploadDemo extends HttpServlet {
 			//迭代集合，找到指定文件
 			for (FileItem fileItem : list) {
 				if(fileItem.isFormField() == false) {
-					File file = new File(realPath + "/" + fileItem.getName());
+					String uuid = UUID.randomUUID().toString().replace("-", "");
+					String filePath = realPath + "/" + uuid + fileItem.getName();
+					File file = new File(filePath);
 					//4.使用FileItem中的write()方法，写到服务器。
 					fileItem.write(file);
 				}
 			}
-		} catch (Exception e) {
+			response.getWriter().write("upload success!!!");
+		}catch (FileSizeLimitExceededException e) {
+			// TODO: handle exception
+			writer.write("文件大小超过2k");
+		}catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-		response.getWriter().write("upload success!!!");
 	}
 
 	
